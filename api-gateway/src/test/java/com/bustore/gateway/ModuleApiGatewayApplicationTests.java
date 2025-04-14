4e262028593a142752d25c223fde8b6a4c885f6c
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-04-14 18:34:02 +0200

    Add initial implementation of API Gateway with Maven configuration and application setup

---
+++ api-gateway/src/test/java/com/bustore/gateway/ModuleApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bustore.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
